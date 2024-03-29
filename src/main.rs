0692d07078bbe523e2ff202a7bd6830e2b27eba2
Paul Sochiera <paul@sochiera.com>
2024-03-29 17:21:13 -0500

    feat: accepts user input via cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-use r_sql::parsing::InputParser;
+use r_sql::binary_writer::BinaryWriter;
+use r_sql::input_parser::InputParser;
+use std::env;
 
 fn main() {
-    InputParser::new(String::from("value"));
+    let query = get_query_from_cli_args();
+    println!("{}", query);
+
+    let input_parser = InputParser::new(query);
+
+    let binary_writer = BinaryWriter::new(input_parser);
+
+    binary_writer.write();
+}
+
+fn get_query_from_cli_args() -> String {
+    let mut args = env::args();
+
+    args.next();
+    args.next()
+        .expect("Please specify one query wrapped in double quotes.")
 }
